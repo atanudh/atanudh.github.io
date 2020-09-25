0ce09ec2a1fbdf08cfc64d843e1887db0bef3a16
atanudh <71276080+atanudh@users.noreply.github.com>
2020-09-25 08:44:21 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -1,22 +1,29 @@
-@media screen and (max-width: 600px) {
-  .menu-bar a:not(:first-child) {display: none;}
-  .menu-bar a.icon {
-    float: right;
-    display: block;
-  }
-}
+// DOM ready
+$(function() {
+ 
+  // Create the dropdown base
+  $("<select />").appendTo("nav");
 
-/* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
-@media screen and (max-width: 600px) {
-  .menu-bar.responsive {position: relative;}
-  .menu-bar.responsive a.icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .menu-bar.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
+  // Create default option "Go to..."
+  $("<option />", {
+    "selected": "selected",
+    "value"   : "",
+    "text"    : "Go to..."
+  }).appendTo("nav select");
+
+  // Populate dropdown with menu items
+  $("nav a").each(function() {
+    var el = $(this);
+    $("<option />", {
+      "value"   : el.attr("href"),
+      "text"    : el.text()
+    }).appendTo("nav select");
+  });
+
+   // To make dropdown actually work
+   // To make more unobtrusive: https://css-tricks.com/4064-unobtrusive-page-changer/
+  $("nav select").change(function() {
+    window.location = $(this).find("option:selected").val();
+  });
+
+});
